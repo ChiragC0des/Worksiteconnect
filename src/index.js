220621d7d84e32d71c44be9eb94723bdb334a1fb
ChiragC0des <poojarichirag98@gmail.com>
2024-08-16 19:01:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'; // Include your custom CSS if needed
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
